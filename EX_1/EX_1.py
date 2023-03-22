35b5b4a68ff556ddd794378360e5cba56daf6c9a
Lopushka <sv13yat@gmail.com>
2023-03-22 21:54:45 +0300

    EX_1_fin_V.1

---
+++ EX_1/EX_1.py
@@ -0,0 +1,5 @@
+print(*sorted(set(input('первый список: ').split(' ')) & set(input('первый список: ').split(' ')), key=int))
+
+# one = set(input('первый список: ').split(' '))
+# two = set(input('первый список: ').split(' '))
+# print(*sorted(one&two))
